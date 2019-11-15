7a8dd1f3a0062af5ffcc0646e1a3f48c3dd54c75
Giorgio Productions <giorgioproductions@gmail.com>
2019-11-15 18:59:07 +0100

    chibi

---
+++ js/chibi.js
@@ -0,0 +1,65 @@
+var resdif = (screen.width/screen.height);
+if (resdif >= 1.6) {
+var Widescreen = 1;
+}
+else {
+var Widescreen = 0;
+}
+if (isMobile.apple.phone || isMobile.android.phone) {
+  console.log("Deja de mirarme desde el móvil")
+  document.getElementById("chibi").classList.remove("chibi");
+  document.getElementById("aboutme").classList.add("aboutmemobile");
+} else {
+  var windowheight = $(window).height();
+  var windowwidth = $(window).width();
+if (windowheight<windowwidth*0.75) {
+console.log("Hola usuario de Windows");
+document.getElementById("aboutme").classList.add("aboutmepc");
+} else {
+console.log("Eres muy raro")
+document.getElementById("chibi").classList.remove("chibi");
+document.getElementById("aboutme").classList.add("aboutmemobile");
+}
+}
+
+var width;
+var height;
+$(window).resize(function() {
+var newWidth = $(window).width();
+var newHeight = $(window).height();
+if( newWidth != width || newHeight != height ) {
+width = newWidth;
+height = newHeight;
+doneResizing();
+}
+});
+
+function doneResizing(){
+  var resdif = (screen.width/screen.height);
+  if (resdif >= 1.6) {
+  var Widescreen = 1;
+  }
+  else {
+  var Widescreen = 0;
+  }
+  if (isMobile.apple.phone || isMobile.android.phone) {
+    console.log("Deja de mirarme desde el móvil")
+    document.getElementById("chibi").classList.remove("chibi");
+    document.getElementById("aboutme").classList.add("aboutmemobile");
+    document.getElementById("aboutme").classList.remove("aboutmepc");
+  } else {
+    var windowheight = $(window).height();
+    var windowwidth = $(window).width();
+  if (windowheight<windowwidth*0.75) {
+  console.log("Hola usuario de Windows");
+  document.getElementById("aboutme").classList.add("aboutmepc");
+  document.getElementById("chibi").classList.add("chibi");
+  document.getElementById("aboutme").classList.remove("aboutmemobile");
+  } else {
+  console.log("Eres muy raro")
+  document.getElementById("aboutme").classList.remove("aboutmepc");
+  document.getElementById("chibi").classList.remove("chibi");
+  document.getElementById("aboutme").classList.add("aboutmemobile");
+  }
+  }
+}
