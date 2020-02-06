239c9d73b7dd2d64fb80d58b04945791251a4fc8
Giorgio Productions <giorgioproductions@gmail.com>
2020-02-06 23:47:26 +0100

    Create giorgio.js

---
+++ js/giorgio.js
@@ -0,0 +1,5 @@
+
+if ((isMobile.apple.phone || isMobile.android.phone) {
+  getElementById("btnvideo").removeAttribute("data-lity");
+  getElementById("btnvideo").setAttribute("target","_blank");
+}
