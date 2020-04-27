a775b8b107bdbd5a4a953b98081579aaba3aa095
Giorgio Productions <giorgioproductions@gmail.com>
2020-04-27 02:01:33 +0200

    changed h3

--- js/giorgio.js
+++ js/giorgio.js
@@ -2,4 +2,6 @@
 if (isMobile.apple.phone || isMobile.android.phone) {
   document.getElementById("btnvideo").removeAttribute("data-lity");
   document.getElementById("btnvideo").setAttribute("target","_blank");
+  document.getElementById("h3").removeAttribute("style");
+  document.getElementById("h3").setAttribute("style","font-size:4vw"");
 }
