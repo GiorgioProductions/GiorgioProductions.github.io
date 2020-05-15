5fa3686bc50dcccad901a7e96d21f3729f8f4533
Giorgio Productions <giorgioproductions@gmail.com>
2020-05-15 17:27:20 +0200

    new design

---
+++ test/new-design/js/giorgio.js
@@ -0,0 +1,4 @@
+if (isMobile.apple.phone || isMobile.android.phone) {
+  document.getElementById("about-me-title").style.display = "none";
+  document.getElementById("about-me-text").setAttribute("style","text-align:center"");
+}
