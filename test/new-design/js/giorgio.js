3d76a00ea1c6949529bbee6ea92fc80a1103a39a
Giorgio Productions <giorgioproductions@gmail.com>
2020-05-15 20:10:20 +0200

    new design updated

--- test/new-design/js/giorgio.js
+++ test/new-design/js/giorgio.js
@@ -1,4 +1,8 @@
 if (isMobile.apple.phone || isMobile.android.phone) {
   document.getElementById("about-me-title").style.display = "none";
-  document.getElementById("about-me-text").setAttribute("style","text-align:center"");
+  document.getElementById("about-me-text").setAttribute("style","text-align:center");
+  document.getElementById("videos").style.display = "none";
+  document.getElementById("discord").style.display = "none";
+  document.getElementById("video").style.display = "none";
+  document.getElementById("hero").setAttribute("style","background-image: url(../images/hero-bg.jpg); background: linear-gradient(rgba(75, 140, 224, 0.65), rgba(172, 75, 225, 0.65)), rgba(0, 0, 0, 0.55) url(../images/hero-bg.jpg) no-repeat;")
 }
